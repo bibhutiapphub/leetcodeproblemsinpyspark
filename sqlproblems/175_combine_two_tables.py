15b52fd1d70fd152e93a6085c26ccd3c458916e6
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2025-09-18 17:47:44 +0530

    combine two tables

--- sqlproblems/175_combine_two_tables.py
+++ sqlproblems/175_combine_two_tables.py
@@ -6,4 +6,6 @@ spark = get_spark_session()
 
 my_data = [(123,"Bibhuti"),(245,"Siddhartha")]
 my_df = spark.createDataFrame(my_data).toDF("id","name")
-my_df.show()
+
+for row in my_df.collect():
+    print(row)
