043ed4981b0435cc3982b875c368a4c0767772fa
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2025-09-19 07:22:19 +0530

    combine two tables

--- sqlproblems/175_combine_two_tables.py
+++ sqlproblems/175_combine_two_tables.py
@@ -4,8 +4,14 @@ from pyspark.sql.functions import *
 
 spark = get_spark_session()
 
-my_data = [(123,"Bibhuti"),(245,"Siddhartha")]
-my_df = spark.createDataFrame(my_data).toDF("id","name")
-
-for row in my_df.collect():
-    print(row)
+person_data = [(1, "Allen", "Wang"), (2, "Bob", "Alice")]
+address_data = [(1, 2, "New York City", "New York"), (2, 3, "Leetcode", "California")]
+person_df = spark.createDataFrame(person_data).toDF("personId", "firstName", "lastName")
+address_df = spark.createDataFrame(address_data).toDF("addressId", "personId", "city", "state")
+join_cond = person_df.personId == address_df.personId
+final_df = (person_df.join(address_df,join_cond,"left")
+                        .select("firstName"
+                                ,"lastName"
+                                ,"city"
+                                ,"state"))
+final_df.show()
