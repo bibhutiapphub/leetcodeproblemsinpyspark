b7674e17cb9fe554d41432dd170aac505578d0c2
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2025-09-21 12:54:30 +0530

    Employees earning more than their managers

--- sqlproblems/178_rank_scores.py
+++ sqlproblems/178_rank_scores.py
@@ -4,14 +4,15 @@ from utils.spark_utils import *
 
 spark = get_spark_session()
 
-my_data = [(1, 3.50)
-    , (2, 3.65)
-    , (3, 4.00)
-    , (4, 3.85)
-    , (5, 4.00)
-    , (6, 3.65)]
+my_data = [(1, "Joe", 70000, 3)
+    , (2, "Henry", 80000, 4)
+    , (3, "Sam", 60000, None)
+    , (4, "Max", 90000, None)]
 
-my_df = spark.createDataFrame(my_data).toDF("id","score")
-partition_window = Window.orderBy(desc("score"))
-final_df = my_df.withColumn("rank",dense_rank().over(partition_window))
+data_df = spark.createDataFrame(my_data).toDF("Id", "Name", "Salary", "MangerId")
+final_df = (data_df.alias("employee_df").join(data_df.alias("manager_df"), expr("employee_df.MangerId == manager_df.Id"))
+            .where(expr("employee_df.Salary > manager_df.Salary"))
+            .select(col("employee_df.Name").alias("Employee"))
+            )
+data_df.show()
 final_df.show()
---
+++ sqlproblems/181_employee_earning_more_than_managers.py
@@ -0,0 +1,5 @@
+from pyspark.sql import Window
+from pyspark.sql.functions import *
+from utils.spark_utils import *
+
+spark = get_spark_session()
